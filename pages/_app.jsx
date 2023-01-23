78528de3066d4591fa0e3922ac052c622f8939db
Neil Mastroianni <suancho@gmail.com>
2023-01-23 18:58:37 -0500

    added cookies

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import Link from 'next/link'
 import Head from 'next/head'
+import Script from 'next/script'
 import { PrismicProvider, PrismicLink } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
 import { repositoryName } from '../prismicio'
 import Heading from '@/components/Heading'
+import { getCookie } from 'cookies-next'
 
 const richTextComponents = {
   heading1: ({ children }) => (
@@ -59,6 +61,7 @@ const richTextComponents = {
 }
 
 export default function App({ Component, pageProps }) {
+  const consent = getCookie(`localConsent`)
   return (
     <PrismicProvider
       internalLinkComponent={(props) => <Link {...props} />}
@@ -85,6 +88,23 @@ export default function App({ Component, pageProps }) {
           />
           <link rel="manifest" href="/site.webmanifest" />
         </Head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('consent', 'default', {'ad_storage': 'denied', 'analytics_storage': 'denied' });(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N7N7XJ9');`}
+        </Script>
+        {consent === true && (
+          <Script
+            id="consupd"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            gtag('consent', 'update', {
+              'ad_storage': 'granted',
+              'analytics_storage': 'granted'
+            });
+          `,
+            }}
+          />
+        )}
         <Component {...pageProps} />
       </PrismicPreview>
     </PrismicProvider>
