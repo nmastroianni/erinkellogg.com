5dbd624bced0491824058f643b3581186e5f9d2e
Neil Mastroianni <nf_mastroianni@hotmail.com>
2023-06-16 15:26:19 -0400

    added robots.txt, updated packages

--- .slicemachine/assets/slices/Content/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Content';
-
-export default {
-  title: 'slices/Content'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"content":[{"type":"paragraph","text":"Et occaecat ea anim incididunt enim quis minim dolore velit eiusmod amet non. Aliquip veniam culpa fugiat elit est labore.","spans":[]}]},"id":"_Default","slice_type":"content"}} />
-_Default.storyName = ''
--- .slicemachine/assets/slices/Divider/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Divider';
-
-export default {
-  title: 'slices/Divider'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"uid":2},"id":"_Default","slice_type":"divider"}} />
-_Default.storyName = ''
